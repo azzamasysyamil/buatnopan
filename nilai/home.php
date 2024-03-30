6577d37fa689e10f25bb4f457bfd7afe6f27008f
Azzam Asy <147239487+azzamasysyamil@users.noreply.github.com>
2024-03-30 13:17:31 +0700

    Add files via upload

---
+++ nilai/home.php
@@ -0,0 +1,8 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+            <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/computer-coding.jpg" width="500px" class="rounded float-end" style="max-width:100%;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit, ante ut blandit varius, risus arcu ornare est, eget dapibus lorem turpis vel ipsum. Nulla in dolor tempor, volutpat odio ut, ullamcorper nisl. Ut porttitor venenatis nulla, ultricies ultrices nisi vulputate vestibulum. Donec sed mi at augue aliquam cursus placerat interdum turpis. Mauris at pellentesque est. Donec congue erat id interdum fringilla. Aliquam elementum sem in dui rutrum, vitae gravida magna accumsan. Duis luctus magna diam, eleifend gravida quam aliquam quis. Nam eget mi volutpat, laoreet ex ac, eleifend quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ex augue, ornare nec lacus auctor, pulvinar maximus ex. Vestibulum nec viverra dolor. Vestibulum varius justo sit amet diam ultricies semper. Aliquam tincidunt laoreet hendrerit.   
+        </p>
+    </div>
+</div>
---
+++ nilai/kelas/add.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header row">
+                <div class="card-title h3 col-8">Tambah Kelas</div>
+                <div class="col-4">
+                    <a href="?m=kelas&s=view" class="btn btn-large btn-primary float-end"><b><</b></a>
+                </div>
+            </div>
+
+            <div class="card-body">
+               <form action="?m=kelas$s=save" method="POST">
+                    <input type="text" name="kelas" class="form-control" placeholder="Nama Kelas" required autofocus>
+               </form>
+            </div>
+        </div>
+    </div>
+</div>
